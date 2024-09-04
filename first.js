66f2e9e60eab4c39a49176563246c6723e4864a1
BHISHMA <bhishmadandekar@gmail.com>
2024-09-04 23:58:45 +0530

    new Projcect

---
+++ first.js
@@ -0,0 +1,16 @@
+console.log("Lets start a new JavaScript Journey");
+//ok that wasnt working but was a good try
+
+const profile = {
+  Photo: "Standard_photo",
+  follow_button: true,
+  message_button: true,
+  post_no: 195,
+  followers: "569k",
+  following: 4,
+  thread: "shardhakahpra",
+  profession: "enterpreanure",
+};
+
+console.log(profile);
+console.log(profile.following);
