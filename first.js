31c5a457ec0e650901606dfa0cf26d230cf972b3
BHISHMA <bhishmadandekar@gmail.com>
2024-09-05 08:31:40 +0530

    Popup Functionality is added

--- first.js
+++ first.js
@@ -1,15 +1,8 @@
 console.log("Lets start a new JavaScript Journey");
 
-const profile = {
-  Photo: "Standard_photo",
-  follow_button: true,
-  message_button: true,
-  post_no: 195,
-  followers: "569k",
-  following: 4,
-  thread: "shardhakahpra",
-  profession: "enterpreanure",
-};
-
-console.log(profile);
-console.log(profile.following);
+inputs = prompt("Enter any number you feel in your mind");
+if (inputs % 5 == 0) {
+  console.log("The number you entered is divisible by 5");
+} else {
+  console.log("The number you entered is not divisible by 5");
+}
